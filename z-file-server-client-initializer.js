d8c97e48d772298df95f34639889a36b9091ce4e
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2016-10-18 13:09:22 +0400

    init

---
+++ z-file-server-client-initializer.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    run: function (app, next) {
+        var ZFileServerClient = require("z-file-server-client");
+        if(app.client){
+            app.client = {};
+        }
+        app.client.zFileServer = new ZFileServerClient(app.config.zFileServerClient);
+        next();
+    }
+};
